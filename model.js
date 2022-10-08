277f5925cf3d138f64cdc8c1df6c7612a774e918
tejasethinos <tejas.kesaria@ethinos.com>
2022-10-08 19:37:31 +0530

    added image field

--- model.js
+++ model.js
@@ -1,37 +1,38 @@
-const mongoose = require('mongoose')
-
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-name:{
-      type: String,
-      required: true,
-    },
-    email:{
-      type: String,
-      required: true,
-    },
-  phonenumber:{
-    type: Number,
-    required: true,
-  },
-  company:{
+  name: {
     type: String,
     required: true,
   },
-  website:{
+  email: {
+    type: String,
+    required: true,
+  },
+  phonenumber: {
+    type: Number,
+    required: true,
+  },
+  company: {
+    type: String,
+    required: true,
+  },
+  website: {
     type: Array,
     required: true,
   },
-    createdat:{
-        type: Date,
-        default: Date.now,
-    },
-    updatedat:{
-        type: Date,
-        default: Date.now,
-    },
-    
-})
+  image: {
+    type: String,
+    required: true,
+  },
+  createdat: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedat: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-module.exports = mongoose.model('User',userSchema);
-
+module.exports = mongoose.model("User", userSchema);
--- server.js
+++ server.js
@@ -1,49 +1,71 @@
-const express=require('express')
-const app=express()
-const mongoose=require('mongoose')
-const cors=require('cors')
-const User=require('./model')
-const userscontroller = require('./controllers')
-const {Namerr} = require('namerr');
-const bodyParser = require('body-parser')
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+const User = require("./model");
+const userscontroller = require("./controllers");
+const bodyParser = require("body-parser");
+const multer = require('multer')
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(express.json())
-const url = 'http://localhost:3000'
-app.use(cors({
-  origin: '*'
-}));
-app.use('/user/',userscontroller)
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+const url = "http://localhost:3000";
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+app.use("/user/", userscontroller);
 
+const ATLAS_URI =
+  "mongodb+srv://rajeshmn47:uni1ver%40se@cluster0.bpxam.mongodb.net/assignmentvouch?retryWrites=true&w=majority";
 
-const ATLAS_URI ='mongodb+srv://rajeshmn47:uni1ver%40se@cluster0.bpxam.mongodb.net/assignment?retryWrites=true&w=majority'
-
-mongoose.Promise = global.Promise
+mongoose.Promise = global.Promise;
 mongoose.connect(
-    ATLAS_URI,
+  ATLAS_URI,
   { useNewUrlParser: true, useUnifiedTopology: true },
   function (error) {
     if (error) {
-      console.log('Error!' + error)
+      console.log("Error!" + error);
     }
   }
-)
-app.get("/",async(req,res)=>{
-    res.send("API running success")
+);
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'public/images')
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name)
+  },
 })
 
-app.get("/pushcategories",async(req,res)=>{
-    res.send("API running")
+const upload = multer({ storage: storage })
+app.post('/api/upload', upload.single('file'), (req, res) => {
+  try {
+    return res.status(200).json('File uploded successfully')
+  } catch (error) {
+    console.error(error)
+  }
 })
-app.get('/createproduct',async(req,res)=>{
-  res.send("API running")
-})
-app.get('/findout',async(req,res)=>{
-    res.send("API running")
-})
-const PORT = process.env.PORT || 9000
+
+app.get("/", async (req, res) => {
+  res.send("API running success");
+});
+
+app.get("/pushcategories", async (req, res) => {
+  res.send("API running");
+});
+app.get("/createproduct", async (req, res) => {
+  res.send("API running");
+});
+app.get("/findout", async (req, res) => {
+  res.send("API running");
+});
+const PORT = process.env.PORT || 9000;
 app.listen(PORT, () => {
-      const firstName =Namerr.name()
-      const email=firstName.split(' ').join('')+'@email.com'
-      console.warn(`App listening on http://localhost:${PORT}`)
-    })
+  console.warn(`App listening on http://localhost:${PORT}`);
+});
